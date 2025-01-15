5e45667d6e09149af41c8216fb7451949e6fc181
dev 4 <dev.4@novel.com>
2025-01-15 10:39:28 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const addEventOnElement=function(elements,eventType,callback){
+    for(let i =0,len=elements.length; i<len;i++){
+        elements[i].addEventListener(eventType,callback)
+    }
+}
+
+const navbar= document.querySelector("[data-navbar]")
+const navToggleBtn=document.querySelector('[data-nav-toggle-btn]')
+const overlay=document.querySelector("[data-overlay]")
+const toggleNavbar=function(){
+    navbar.classList.toggle("active")
+    navToggleBtn.classList.toggle("active")
+    overlay.classList.toggle("active")
+    document.body.classList.toggle("nav-active")
+}
+
+addEventOnElement([navToggleBtn,overlay],"click",toggleNavbar)
+
+
+const parallaxElements=document.querySelectorAll("[data-parallax]");
+window.addEventListener("mousemove",event=>{
+    for(let i=0,len=parallaxElements.length ;i<len ; i++){
+        const movementX = (event.clientX/window.innerWidth)*Number(parallaxElements[i].dataset.parallaxSpeed);
+        const movementy=(event.clienty/window.innerHeight)*Number(parallaxElements[i].dataset.parallaxSpeed);
+       
+        parallaxElements[i].animate({
+            transform:`translate(${movementX}px,${movementy}px)`
+        },{duration:500 ,fill:"forwards"})
+    
+    }
+})
